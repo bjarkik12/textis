6064ea19bc12c6243c98a18f517e912a09e90edd
hrannar10 <hrannar10@ru.is>
2014-05-07 18:32:59 +0000

    Uppfærði bootstrap og breytti útliti í "Justified-Nav"

--- textis/App_Start/BundleConfig.cs
+++ textis/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace textis
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/justified-nav.css"));
         }
     }
 }
