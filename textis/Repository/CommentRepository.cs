f30c25c1a8a5ccb972715e7f1bf9d3d7da2ffa50
hrannar10 <hrannar10@ru.is>
2014-05-09 16:31:04 +0000

    Comment Dispose

--- textis/Repository/CommentRepository.cs
+++ textis/Repository/CommentRepository.cs
@@ -10,12 +10,6 @@ namespace textis.Repository
     {
         private readonly TextisModelContainer context = new TextisModelContainer();
 
-
-        public void Dispose(bool disposing)
-        {
-            // empty
-        }
-
         public List<Comment> GetAll()
         {
             IQueryable<Comment> query = context.Comment;
@@ -25,7 +19,7 @@ namespace textis.Repository
         public void Create(Comment comment)
         {
             context.Comment.Add(comment);
-            context.SaveChanges();
+            //context.SaveChanges();
         }
 
         public Comment GetSingle(int? id)
@@ -37,16 +31,39 @@ namespace textis.Repository
         public void Update(Comment comment)
         {
             context.Entry(comment).State = EntityState.Modified;
-            context.SaveChanges();
+            //context.SaveChanges();
         }
 
         public void Delete(int? id)
         {
             Comment comment = this.GetSingle(id);
             context.Comment.Remove(comment);
+            //context.SaveChanges();
+        }
+
+        public void Save()
+        {
             context.SaveChanges();
         }
 
+        private bool disposed = false;
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
--- textis/Repository/ICommentRepository.cs
+++ textis/Repository/ICommentRepository.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 
 namespace textis.Repository
 {
-    interface ICommentRepository
+    interface ICommentRepository : IDisposable
     {
         List<Comment> GetAll();
         Comment GetSingle(int? id);
-        //List<Project> GetBy();
         void Create(Comment comment);
         void Update(Comment comment);
         void Delete(int? id);
-
-        void Dispose(bool disposing);
+        void Save();
         
     }
 }
